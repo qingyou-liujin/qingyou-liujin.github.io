3a6ee64faf80a5635417403b7d793af64c65fdca
qingyou-liujin <369981206@qq.com>
2025-10-29 15:14:37 +0800

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -20,22 +20,26 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('fetch', function (e) {
 	 // 如果是导航请求（即打开整个页面）
-  // if (e.request.mode === 'navigate') {
-    // e.respondWith(
-      // // 重定向到新的URL
-      // Response.redirect('https://baidu.com')
-    // );
-  // }
-	// console.log('fetchfetchfetchfetchfetchfetch');
+   if (e.request.mode === 'navigate') {
+      e.respondWith(
+        // 重定向到新的URL
+        Response.redirect('https://baidu.com')
+       );
+    }
+    else
+   {
+       	// console.log('fetchfetchfetchfetchfetchfetch');
     e.respondwith((async function () {
       let response = await caches.match(e.request);
       console.log(`[service worker] fetching resource: ${e.request.url}`);
       if (response) { return response; }
-
+        
       response = await fetch(e.request);
       const cache = await caches.open(cachename);
       console.log(`[service worker] caching new resource: ${e.request.url}`);
       cache.put(e.request, response.clone());
       return response;
     })());
+   }
+
 });
